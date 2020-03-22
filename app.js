883f87511796b0d1053f20c11262b715be463951
Emma <enabojohn@gmail.com>
2020-03-22 18:21:44 +0300

    Added a Http post request and  validation

--- app.js
+++ app.js
@@ -25,12 +25,39 @@ app.get('/api/genres/:id', (req,res) => {
     res.send(genre);
 });
 
+//POST
+app.post('/api/genres/', (req, res) => {
+    const { error } = validateGenre(req.body);//Object destructuring syntax
+    if( error ) return res.status(400).send(error.details[0].message); 
+    
+    /*
+    *Read the genre object
+    *Use its propertise to create a new genre object
+    *Then add the new genre object to the genre array
+    */
+    const genre = {
+        //get the number of elements in the genre array, add one to it
+        id: genres.length + 1,
+        name: req.body.name
+    };
+    // Push the genre object to the array of genres
+    genres.push(genre);
+    res.send(genre);
+});
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
 });
 
+//better way of validating would be
+function validateGenre(genre) {
+    const schema  = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(genre, schema);
+}
 
 
 
