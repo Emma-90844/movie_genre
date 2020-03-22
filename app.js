6d2297d871b1bea783ce81a031640c766f5e33fa
Emma <enabojohn@gmail.com>
2020-03-22 23:26:45 +0300

    Added the route folder with homeRoute.js and genres.js route files

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const dbDebugger = require('debug')('app:db');
 const morgan = require('morgan');
 const Joi = require('joi');
 
+const genres = require('./routes/genres');
+const homeRoute = require('./routes/homeRoute');
 
 // const logger = require('./logger');
 // const auth = require('./athenticate');
@@ -14,17 +16,19 @@ const app = express();
 //Setting templating engine
 app.set('view engine', 'pug');
 app.set('views', './views');
-
+app.use(express.urlencoded({ extended : true }));
 
 app.use(express.json());
 //last middle ware is static: serve statis files
 app.use(express.static('public'));
 //Morgan logs http request
 app.use(morgan('tiny'));
-app.use(express.urlencoded({ extended : true }));
+
 //Middleware functions that set HTTP response headers.
 app.use(helmet());
 
+app.use('/api/genres', genres);
+app.use('/', homeRoute);
 
 //Defining working environment/ development, staging,testing/
 if(app.get('env') === 'development'){
@@ -48,78 +52,6 @@ app.use(function auth (req, res, next) {
 });
 
 
-const genres = [
-    {id:1, name : 'Adventure' },
-    {id:2, name : 'Chinese' },
-    {id:3, name : 'Detective' },
-    {id:4, name : 'Christian' },
-];
-
-app.get('/',(req, res) => {
-    res.render('index',{title: 'My Express App', message: 'Hello'})
-});
-
-//GET(  Returns a list of genres )
-app.get('/api/genres', (req, res) => {
-    res.send(genres);
-});
-
-//GET(  Returns a single movie with specific id   )
-app.get('/api/genres/:id', (req,res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
-    if( !genre ) return res.status(404).send('Error:Movie Genre with the given id not found');
-    res.send(genre);
-});
-
-//POST
-app.post('/api/genres/', (req, res) => {
-    const { error } = validateGenre(req.body);//Object destructuring syntax
-    if( error ) return res.status(400).send(error.details[0].message); 
-    
-    /*
-    *Read the genre object
-    *Use its propertise to create a new genre object
-    *Then add the new genre object to the genre array
-    */
-    const genre = {
-        //get the number of elements in the genre array, add one to it
-        id: genres.length + 1,
-        name: req.body.name
-    };
-    // Push the genre object to the array of genres
-    genres.push(genre);
-    res.send(genre);
-});
-
-
-//PUT(UPDATE)
-//Because we are dealing with a spoecific course:we need need a course id
-app.put('/api/genres/:id', (req,res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
-    if( !genre ) return res.status(404).send('Error: Genre with the given id not found');
-    const { error } = validateGenre(req.body);  //Object destructuring feature
-    if( error ){
-        res.status(400).send(error.details[0].message); 
-        return; 
-    } 
-    //Look through find if genre exist
-    genre.name = req.body.name;
-    res.send(genre);
-});
-
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Listening on port ${port}...`);
-});
-
-//better way of validating would be
-function validateGenre(genre) {
-    const schema  = {
-        name: Joi.string().min(3).required()
-    };
-    return Joi.validate(genre, schema);
-}
 
 
 
@@ -128,6 +60,7 @@ function validateGenre(genre) {
 
 
 
+module.exports = router;
 
 
 
---
+++ routes/genres.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+
+
+const genres = [
+    {id:1, name : 'Adventure' },
+    {id:2, name : 'Chinese' },
+    {id:3, name : 'Detective' },
+    {id:4, name : 'Christian' },
+];
+//GET(  Returns a list of genres )
+router.get('/', (req, res) => {
+    res.send(genres);
+});
+
+//GET(  Returns a single movie with specific id   )
+router.get('/:id', (req,res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if( !genre ) return res.status(404).send('Error:Movie Genre with the given id not found');
+    res.send(genre);
+});
+
+//POST
+router.post('/', (req, res) => {
+    const { error } = validateGenre(req.body);//Object destructuring syntax
+    if( error ) return res.status(400).send(error.details[0].message); 
+    
+    /*
+    *Read the genre object
+    *Use its propertise to create a new genre object
+    *Then add the new genre object to the genre array
+    */
+    const genre = {
+        //get the number of elements in the genre array, add one to it
+        id: genres.length + 1,
+        name: req.body.name
+    };
+    // Push the genre object to the array of genres
+    genres.push(genre);
+    res.send(genre);
+});
+
+
+//PUT(UPDATE)
+//Because we are dealing with a spoecific course:we need need a course id
+router.put('/:id', (req,res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if( !genre ) return res.status(404).send('Error: Genre with the given id not found');
+    const { error } = validateGenre(req.body);  //Object destructuring feature
+    if( error ){
+        res.status(400).send(error.details[0].message); 
+        return; 
+    } 
+    //Look through find if genre exist
+    genre.name = req.body.name;
+    res.send(genre);
+});
+
+
+const port = process.env.PORT || 5000;
+router.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
+
+//better way of validating would be
+function validateGenre(genre) {
+    const schema  = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(genre, schema);
+}
